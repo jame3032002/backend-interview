14aeaee473dc046598c9115a98bd4561e07fd014
jame3032002 <jame3032002@hotmail.com>
2024-02-17 17:50:55 +0700

    :zap: Added default value of accessTokenSecretKey

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,4 +1,5 @@
-const accessTokenSecretKey = process.env.ACCESS_TOKEN_SECRET_KEY;
+const accessTokenSecretKey =
+  process.env.ACCESS_TOKEN_SECRET_KEY || "ACCESS_TOKEN_SECRET_KEY";
 const RATE_LIMIT_TIME_RESET = process.env.RATE_LIMIT_TIME_RESET || 1;
 const RATE_LIMIT_REQUEST_PER_TIME_RESET =
   process.env.RATE_LIMIT_REQUEST_PER_TIME_RESET || 20;
